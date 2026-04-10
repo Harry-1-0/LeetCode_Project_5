d9b6df72c07af9eb56beaf55136f104f4c2882bb
Harry-1-0 <datamininglab1234@gmail.com>
2026-04-10 15:51:28 +0800

    Create Solution-C++02.cpp

---
+++ src/Solution-C++02.cpp
@@ -0,0 +1,16 @@
+// Dynamic Programming Solution
+
+// 1. Create a dp array where dp[i] represents the maximum subarray sum ending at index i
+// 2. For each index i, dp[i] is the maximum of nums[i] and nums[i] + dp[i - 1]
+// 3. The answer is the maximum value in the dp array
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> dp(nums);
+        for (int i = 1; i < nums.size(); i++) {
+            dp[i] = max(nums[i], nums[i] + dp[i - 1]);
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
